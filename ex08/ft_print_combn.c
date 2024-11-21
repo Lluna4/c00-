c3480f0b368dddf550ba506cf500d9dc58c406bd
Luna <69774917+Lluna4@users.noreply.github.com>
2024-11-21 04:16:28 +0100

    Add files via upload

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lunatran <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: luna <luna@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 16:50:51 by lunatran          #+#    #+#             */
-/*   Updated: 2024/11/13 19:28:32 by lunatran         ###   ########.fr       */
+/*   Updated: 2024/11/20 22:00:17 by luna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,50 +50,90 @@ void ft_putnbr(int nb)
     ft_putchar(nb + '0');
 }
 
-void ft_print_numbers(int *a)
+void ft_print_numbers(int *a, size_t size)
 {
-	while (*a != 0)
+	
+	while (size > 0)
 	{
-		if (*a < 10)
-			ft_putnbr(0);
 		ft_putnbr(*a);
+		
 		a++;
+		size--;
 	}
+	ft_putstr(", ");
 }
 
-
+//state 0 is sum last number 
+//state 1 is sum other numbers pointed by index2 
+//state 2 is break
 void ft_print_combn(int n)
 {
-    	int 	a[10];
+    int 	a[10];
 	int 	index;
-	int	max_index;
+	int		index2;
+	int		prev_index;
+	int		max_index;
+	int 	state;
 
 	index = 0;
-    	while (index < n)
+	state = 0;
+	prev_index = 0;
+    while (index < n)
 	{
 		a[index] = index;
 		index++;
 	}
+	index--;
 	max_index = index;
-	ft_putstr("Done");
+	index2 = index - 1;
+	ft_putstr("Done\n");
 	while(1)
 	{
-		if (index - 1 < 9 - (max_index - index))
+		if (state == 0)
 		{
-			ft_print_numbers(a);
+			if(a[index] > 9)
+			{
+				state = 1;
+				continue;
+			}
+			ft_print_numbers(a, max_index + 1);
 			a[index]++;
 		}
-		else
+		if (state == 1)
 		{
-			index--;
+			while (1)
+			{
+				if (index2 == -1)
+				{
+					state = 2;
+					break;
+				}
+				prev_index = index2;
+				a[index2]++;
+				while (index2 < max_index)
+				{
+					a[index2 + 1] = a[index2] + 1;
+					index2++;
+				}
+				index2 = prev_index;
+				if (a[index2] > (9 - (max_index - index2)))
+				{
+					index2--;
+				}
+				else
+				{
+					state = 0;
+					index2 = index - 1;
+					break;
+				}
+			}
 		}
-		if (index < 0)
+		if (state == 2)
 			break;
 	}	
-
 }
 
 int main()
 {
-    ft_print_combn(2);
+    ft_print_combn(5);
 }
